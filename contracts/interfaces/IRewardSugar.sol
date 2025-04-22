cc148e274531691fbb1da81caa3a42c360f36efc
luan-ixs <luan@ixswap.io>
2025-04-22 11:17:46 +0700

    get reward tokens from Reward contract instead of WeightedPool contract

--- contracts/interfaces/IRewardSugar.sol
+++ contracts/interfaces/IRewardSugar.sol
@@ -30,25 +30,27 @@ interface IRewardSugar {
      * @notice Get the earned fees and bribes for a specific pool and tokenId
      * @param _pool The address of the pool
      * @param _tokenId The lock Id of the user
-     * @return tokens The addresses of the tokens in the pool
+     * @return feeTokens The addresses of the fee reward tokens in the pool
+     * @return bribeTokens The addresses of the bribe reward tokens in the pool
      * @return fees The amounts of the fees earned
      * @return bribes The amounts of the bribes earned
      */
     function getFeeAndBribeVotingRewards(
         address _pool,
         uint256 _tokenId
-    ) external view returns (address[] memory tokens, uint256[] memory fees, uint256[] memory bribes);
+    ) external view returns (address[] memory feeTokens, address[] memory bribeTokens, uint256[] memory fees, uint256[] memory bribes);
 
     /*
      * @notice Get the earned fees and bribes for multiple pools and tokenId
      * @param _pools The addresses of the pools
      * @param _tokenId The lock Id of the user
-     * @return tokens The addresses of the tokens in the pool
+     * @return tokens The addresses of the fee reward tokens in the pool
+     * @return bribeTokens The addresses of the bribe reward tokens in the pool
      * @return fees The amounts of the fees earned
      * @return bribes The amounts of the bribes earned
      */
     function getFeeAndBribeVotingRewardsForTokenIdAndPools(
         address[] calldata _pools,
         uint256 _tokenId
-    ) external view returns (address[][] memory tokens, uint256[][] memory fees, uint256[][] memory bribes);
+    ) external view returns (address[][] memory feeTokens, address[][] memory bribeTokens, uint[][] memory feeRewards, uint[][] memory bribeRewards);
 }
